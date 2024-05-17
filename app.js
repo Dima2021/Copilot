069636b403a6cb2f69f80e17d19e2c295dae0288
Dima2021 <61808880+Dima2021@users.noreply.github.com>
2024-05-17 09:03:42 -0500

    Test

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var msg = 'Hello World';
 console.log(msg);
 
-function calculateDaysBetweenDates(begin, end) {
-    const millisecondsInDay = 86400000;
-    const beginDate = new Date(begin);
-    const endDate = new Date(end);
-    const difference = endDate - beginDate;
-    return Math.round(difference / millisecondsInDay);
+function calculateDaysBetweenDates(date1, date2) {
+    const oneDay = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
+    const firstDate = new Date(date1);
+    const secondDate = new Date(date2);
+    const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
+    return diffDays;
 }
+
+const date1 = new Date('2024-01-01');
+const date2 = new Date('2024-01-10');
+const daysBetweenDates = calculateDaysBetweenDates(date1, date2);
+console.log(daysBetweenDates);
