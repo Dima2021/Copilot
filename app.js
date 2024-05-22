e73a6a42d87ea8da0bcbb155729bed58858a0e51
Dima2021 <dima.gorbonos@whitesourcesoftware.com>
2024-05-22 11:57:16 -0500

    Removed function calculateDistanceBetween2Points

--- app.js
+++ app.js
@@ -25,7 +25,3 @@ function calculateTimeBetweenDates(date1, date2) {
 }
 
 
-function calculateDistanceBetween2Points(x1, y1, x2, y2) {
-    const distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-    return distance;
-}
