400c23e2b3fd45d4450c2f3d2f5eedae0ad81fe7
Dima2021 <dima.gorbonos@whitesourcesoftware.com>
2024-05-22 11:30:11 -0500

    function calculateDaysBetweenDates

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var msg = 'Hello World';
 console.log(msg);
 
-function calculateTimeBetweenDates
-(date1, date2) => {
+function calculateTimeBetweenDates(date1, date2) => {
     const diffInMilliseconds = Math.abs(date2 - date1);
     const diffInSeconds = Math.floor(diffInMilliseconds / 1000);
     const diffInMinutes = Math.floor(diffInSeconds / 60);
@@ -18,3 +17,23 @@ function calculateTimeBetweenDates
     };
 }
 
+function calculateTimeBetweenDates(date1, date2) {
+    const diffInMilliseconds = Math.abs(date2 - date1);
+    const diffInDays = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24));
+    
+    return diffInDays;
+}
+
+
+function calculateDaysBetweenDates(date1, date2) {
+    const diffInMilliseconds = Math.abs(date2 - date1);
+    const diffInDays = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24));
+    
+    return diffInDays;
+}
+
+const startDate = new Date('2022-01-01');
+const endDate = new Date('2022-12-31');
+
+const daysBetweenDates = calculateDaysBetweenDates(startDate, endDate);
+console.log(`Days between dates: ${daysBetweenDates}`);
